bf3cbcc969a7f4bfe1094c59c3d66634e58e29b5
amit-raja <54271074+amit-raja@users.noreply.github.com>
2020-10-28 16:28:06 +0530

    Update models.py

--- twitter_clone/new_twitter/models.py
+++ twitter_clone/new_twitter/models.py
@@ -5,15 +5,16 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class userFollowing(models.Model):
+    username=models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+    #following= models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
+    #followers = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
+    followers=models.ManyToManyField(User,related_name='follow',blank=True)
+    #follow_time = models.DateTimeField(auto_now_add=True)
 
-    following= models.ForeignKey(User, related_name="following", on_delete=models.CASCADE)
-    followers = models.ForeignKey(User, related_name="followers", on_delete=models.CASCADE)
-    follow_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
+    '''class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['following','follower'],  name="unique_followers")
-        ]
+            models.UniqueConstraint(fields=[""],  name="unique_followers")
+        ]'''
 
         #ordering = ["-created"]
 
@@ -23,9 +24,11 @@ class userFollowing(models.Model):
 
 class user_detail(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_id")
-    user_name = models.CharField(max_length=40)
+    #user_name = models.CharField(max_length=40)
     tweets = models.TextField(blank=True, null=True)
     likes = models.ManyToManyField(User, related_name='tweet_like', blank=True)
-    retweeted = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    #retweeted = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    retweets=models.ManyToManyField(User,related_name='retweeted',blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    #= models.DateTimeField(auto_now_add=True)
     #retweet = models.BooleanField(default=False)
